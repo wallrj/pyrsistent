1695272e4bc822d04063af43bf8035c13a0f8e9e
Bridget McErlean <bridgetmcerlean@gmail.com>
2016-03-24 17:32:29 +0000

    Store the hash of a PClass when an instance is created.
    Attempt to store the hash of a PClass instance. If calculating the
    hash fails for any reason, fall back to calculating it every time
    __hash__ is called.

--- pyrsistent/_pclass.py
+++ pyrsistent/_pclass.py
@@ -13,7 +13,7 @@ class PClassMeta(type):
     def __new__(mcs, name, bases, dct):
         set_fields(dct, bases, name='_pclass_fields')
         store_invariants(dct, bases, '_pclass_invariants', '__invariant__')
-        dct['__slots__'] = ('_pclass_frozen',) + tuple(key for key in dct['_pclass_fields'])
+        dct['__slots__'] = ('_pclass_frozen','_hash',) + tuple(key for key in dct['_pclass_fields'])
 
         # There must only be one __weakref__ entry in the inheritance hierarchy,
         # lets put it on the top level class.
@@ -63,6 +63,10 @@ class PClass(CheckedType):
 
         check_global_invariants(result, cls._pclass_invariants)
 
+        try:
+            result._hash = hash(tuple((key, getattr(result, key, _MISSING_VALUE)) for key in result._pclass_fields))
+        except:
+           result._hash = None
         result._pclass_frozen = True
         return result
 
@@ -144,7 +148,9 @@ class PClass(CheckedType):
 
     def __hash__(self):
         # May want to optimize this by caching the hash somehow
-        return hash(tuple((key, getattr(self, key, _MISSING_VALUE)) for key in self._pclass_fields))
+        if not self._hash:
+            return hash(tuple((key, getattr(self, key, _MISSING_VALUE)) for key in self._pclass_fields))
+        return self._hash
 
     def __setattr__(self, key, value):
         if getattr(self, '_pclass_frozen', False):
